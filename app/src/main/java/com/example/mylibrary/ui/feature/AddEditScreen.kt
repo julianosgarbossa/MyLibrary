91374aa0977dbeaa557b3880179bd928e2d483b6
Juliano Sgarbossa <contato.julianosgarbossa@gmail.com>
2025-10-22 12:00:57 -0300

    AddEditScreen

---
+++ app/src/main/java/com/example/mylibrary/ui/feature/AddEditScreen.kt
@@ -0,0 +1,72 @@
+package com.example.mylibrary.ui.feature
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mylibrary.ui.theme.MyLibraryTheme
+
+@Composable
+fun AddEditScreen() {
+    AddEditContent()
+}
+
+@Composable
+fun AddEditContent() {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { }) {
+                Icon(Icons.Default.Check, contentDescription = "Save")
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .consumeWindowInsets(paddingValues)
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                value = "",
+                onValueChange = { },
+                placeholder = {
+                    Text(text = "Título")
+                }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                value = "",
+                onValueChange = { },
+                placeholder = {
+                    Text(text = "Autor")
+                }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AddEditContentPrev() {
+    MyLibraryTheme {
+        AddEditContent()
+    }
+}
